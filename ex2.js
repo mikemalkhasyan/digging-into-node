cdda88bc704b76529d706bf6a0f1b81341bd00ab
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2021-02-02 16:06:49 +0400

    Streams: gzip Compress with zlib

--- ex2.js
+++ ex2.js
@@ -1,22 +1,25 @@
 #!/usr/bin/env node
 
+// node ex2.js --file=files/lorem.txt --compress
+
 "use strict";
 
 const path = require("path");
 const fs = require("fs");
 const Transform = require('stream').Transform;
+const zlib = require('zlib');
 
 // var getStdin = require("get-stdin");
 
 const args = require("minimist")(process.argv.slice(2),{
-    boolean: ["help","in", "out"],
+    boolean: ["help","in", "out", "compress"],
     string: ["file",],
 });
 
 const BASE_PATH =
     path.resolve(process.env.BASEPATH || __dirname);
 
-const OUTFILE = path.join(BASE_PATH, "out.txt");
+let OUTFILE = path.join(BASE_PATH, "out.txt");
 
 if (args.help || process.argv.length <= 2) {
     error(null,/*showHelp=*/true);
@@ -56,6 +59,12 @@ function processFile(inStream) {
 
     outStream = outStream.pipe(upperStream);
 
+    if (args.compress) {
+        let gzipStream = zlib.createGzip();
+        outStream = outStream.pipe( gzipStream );
+        OUTFILE = `${OUTFILE}.gz`;
+    }
+
     let targetStream;
 
     if (args.out) {
@@ -75,6 +84,7 @@ function printHelp() {
     console.log("--file={FILENAME}  process the file");
     console.log("--in, -            process stdin");
     console.log("--out              print to stdout");
+    console.log("--compress         gzip the output");
     console.log("");
 }
 
