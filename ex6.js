b342eabc7b078a38bf89f4a649ae57b67b787207
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2021-02-04 21:23:13 +0400

    Web Servers: Custom Routing Middleware

--- ex6.js
+++ ex6.js
@@ -52,6 +52,7 @@ function main() {
 
 function defineRoutes() {
 	app.get("/get-records", async function (req, res) {
+		await delay(1000);
 		const records = await getAllRecords();
 		res.writeHead(200, {
 			"Content-Type": "application/json",
@@ -60,39 +61,26 @@ function defineRoutes() {
 		res.end(JSON.stringify(records));
 	});
 
+	app.use(function (req, res, next) {
+		if (/^\/(?:index\/?)?(?:[?#].*$)?$/.test(req.url)) {
+			req.url = "/index.html";
+		} else if (/^\/js\/.+$/.test(req.url)) {
+			next();
+			return;
+		} else if (/^\/(?:[\w\d]+)(?:[\/?#].*$)?$/.test(req.url)) {
+			let [,basename] = req.url.match(/^\/([\w\d]+)(?:[\/?#].*$)?$/);
+			req.url = `${basename}.html`;
+		}
+
+		next();
+	});
+
 	app.use(express.static(WEB_PATH, {
 		maxAge: 100,
 		setHeaders: function setHeaders(res) {
 			res.setHeader("Server", "Node Workshop: ex6");
 		}
 	}));
-
-
-	// TODO: define routes
-	//
-	// Hints:
-	//
-	// {
-	// 	match: /^\/(?:index\/?)?(?:[?#].*$)?$/,
-	// 	serve: "index.html",
-	// 	force: true,
-	// },
-	// {
-	// 	match: /^\/js\/.+$/,
-	// 	serve: "<% absPath %>",
-	// 	force: true,
-	// },
-	// {
-	// 	match: /^\/(?:[\w\d]+)(?:[\/?#].*$)?$/,
-	// 	serve: function onMatch(params) {
-	// 		return `${params.basename}.html`;
-	// 	},
-	// },
-	// {
-	// 	match: /[^]/,
-	// 	serve: "404.html",
-	// },
-
 }
 
 // *************************
