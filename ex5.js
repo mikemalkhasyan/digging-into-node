d0bed2796d0817d9085f904f6f05630b3cc975d0
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2021-02-04 20:41:36 +0400

    Web Servers: Routing & Serving Static Files

--- ex5.js
+++ ex5.js
@@ -35,12 +35,32 @@ var SQL3 = {
 	exec: util.promisify(myDB.exec.bind(myDB)),
 };
 
-// var fileServer = new staticAlias.Server(WEB_PATH,{
-// 	cache: 100,
-// 	serverInfo: "Node Workshop: ex5",
-// 	alias: [
-// 	],
-// });
+var fileServer = new staticAlias.Server(WEB_PATH,{
+	cache: 100,
+	serverInfo: "Node Workshop: ex5",
+	alias: [
+		{
+			match: /^\/(?:index\/?)?(?:[?#].*$)?$/,
+			serve: "index.html",
+			force: true,
+		},
+		{
+			match: /^\/js\/.+$/,
+			serve: "<% absPath %>",
+			force: true,
+		},
+		{
+			match: /^\/(?:[\w\d]+)(?:[\/?#].*$)?$/,
+			serve: function onMatch(params) {
+				return `${params.basename}.html`;
+			},
+		},
+		{
+			match: /[^]/,
+			serve: "404.html",
+		}
+	],
+});
 
 const httpserv = http.createServer(handleRequest);
 
@@ -55,13 +75,7 @@ function main() {
 }
 
 async function handleRequest(req, res) {
-	if (req.url === '/hello') {
-		res.writeHead(200, { "Content-Type": "text/plain" });
-		res.end("Hello World");
-	} else {
-		res.writeHead(404);
-		res.end();
-	}
+	fileServer.serve(req, res);
 }
 
 // *************************
